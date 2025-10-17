d2533e92a17605d82c9abd7ceaf5695dc4f29b67
Karryl Dumalag <kjdumalag@gmail.com>
2025-10-17 08:04:48 -0700

    Import badge for project labels

---
+++ app/pages/Welcome/welcome.tsx
@@ -0,0 +1,27 @@
+import logoDark from "./logo-dark.svg";
+import logoLight from "./logo-light.svg";
+import { Badge } from "@/app/components/ui/badge";
+
+export function Welcome() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+        <header className="flex flex-col items-center gap-9">
+          <div className="w-[500px] max-w-[100vw] p-4">
+            <Badge variant="default">Badge</Badge>
+            <img
+              src={logoLight}
+              alt="React Router"
+              className="block w-full dark:hidden"
+            />
+            <img
+              src={logoDark}
+              alt="React Router"
+              className="hidden w-full dark:block"
+            />
+          </div>
+        </header>
+      </div>
+    </main>
+  );
+}
